d32e3da315f3a61651b726aceb113051faf375d5
RayaneBGit <rayane.badaoui04@gmail.com>
2025-06-28 20:24:37 -0400

    essai 1

---
+++ src/Accueil.jsx
@@ -0,0 +1,11 @@
+import './Accueil.css';
+
+function Accueil() {
+  return (
+      <header>
+        <p>helklo</p>
+      </header>
+  );
+}
+
+export default Accueil;
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Accueil from './Accueil';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Accueil></Accueil>
+    </div>
+  );
+}
+
+export default App;
