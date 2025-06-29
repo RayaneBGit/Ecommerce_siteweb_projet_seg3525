1727862380c95ce526dc5a6bd0904ec0801fdb45
RayaneBGit <rayane.badaoui04@gmail.com>
2025-06-29 00:23:03 -0400

    Premier partie page accueil, création de header et banniere pagge accueil

--- src/Accueil.jsx
+++ src/Accueil.jsx
@@ -1,10 +1,23 @@
-import './Accueil.css';
 
+import './Accueil.css';
 function Accueil() {
   return (
-      <header>
-        <p>helklo</p>
-      </header>
+    <main>
+        <section className="hero_presentation">
+           <div className="banniere_page_accueil"   style={
+            
+            {
+            backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${process.env.PUBLIC_URL}/images/maison_accueil_page_variation2.png)`
+            }
+  
+            }>
+            <div className="div_titre_page_accueil_banniere">
+            <h1 className="titre_page_accueil_banniere">Des portes et des fenètres pour un intérieur chaud et sécurisant</h1>
+            
+            </div>
+           </div>
+        </section>
+    </main>
   );
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,37 @@ import './App.css';
 function App() {
   return (
     <div>
+      <header>
+        <nav>
+            <ul>
+                <li>
+                    <a className="lien_header">
+                    Accueil
+                    </a>
+                </li>
+                <li>
+                    <a className="lien_header">
+                    Panier
+                    </a>
+                </li>
+                 <li>
+                    <a className="lien_header">
+                    Promotion
+                    </a>
+                </li>
+                <li>
+                    <a className="lien_header">
+                    Contact
+                    </a>
+                </li>
+                <li>
+                    <a className="lien_header">
+                    À propos
+                    </a>
+                </li>
+            </ul>
+        </nav>
+      </header>
       <Accueil></Accueil>
     </div>
   );
