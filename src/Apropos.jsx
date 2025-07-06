34fc38df18ba6fbb19587cd9c0084b845348ed80
Yasmine-bd <145608686+Yasmine-bd@users.noreply.github.com>
2025-07-06 18:12:11 -0400

    Add files via upload

--- src/Apropos.jsx
+++ src/Apropos.jsx
@@ -3,6 +3,9 @@ function Apropos() {
     <div style={{ padding: '20px' }}>
       <h1>À propos de nous</h1>
       <p>
+        <blockquote>
+        Portes & Fenêtres Élégance
+      </blockquote>
         Depuis 1999, nous concevons et installons des portes et fenêtres de qualité au Canada. Notre
         mission est de fournir des produits durables, esthétiques et adaptés au climat canadien.
         Grâce à notre équipe passionnée, nous accompagnons nos clients dans la réalisation de leurs projets.
